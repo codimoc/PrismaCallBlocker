a7ef17d5df449190cc20ce194a3e83e8aa4f5467
ConteDiMonteCristo <gchiaia@prismaqf.com>
2016-02-11 18:28:59 +0000

    ShowServiceRuns now use the CursorLoader / SimpleCursorAdapter pattern. Also added styles (rounded)

--- app/src/main/java/com/prismaqf/callblocker/ShowServiceRuns.java
+++ app/src/main/java/com/prismaqf/callblocker/ShowServiceRuns.java
@@ -1,17 +1,15 @@
 package com.prismaqf.callblocker;
 
 import android.app.ListActivity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.v4.widget.SimpleCursorAdapter;
-import android.util.Log;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.prismaqf.callblocker.sql.DbContract;
 import com.prismaqf.callblocker.sql.DbHelper;
@@ -23,65 +21,76 @@ import com.prismaqf.callblocker.sql.ServiceRun;
  * and event triggered
  * @author ConteDiMonteCristo
  */
-public class ShowServiceRuns extends ListActivity {
+public class ShowServiceRuns extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private final String TAG = ShowServiceRuns.class.getCanonicalName();
-    private CallDetectService myService;
-    private boolean isBound = false;
-    private SQLiteDatabase myDb;
-
-
-    private ServiceConnection myConnection = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            CallDetectService.LocalBinder binder = (CallDetectService.LocalBinder) service;
-            myService = binder.getService();
-            isBound = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            isBound = false;
-        }
-    };
+    private static final int URL_LOADER = 0; // Identifies a particular Loader being used in this component
+    private SimpleCursorAdapter myAdapter;
+    private SQLiteDatabase myDbConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.service_runs_activity);
-    }
+        myDbConnection = new DbHelper(this).getReadableDatabase();
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+        myAdapter = new SimpleCursorAdapter(this,
+                R.layout.service_run_record,
+                null,  //no cursor yet
+                new String[] {DbContract.ServiceRuns.COLUMN_NAME_START,
+                        DbContract.ServiceRuns.COLUMN_NAME_STOP,
+                        DbContract.ServiceRuns.COLUMN_NAME_TOTAL_RECEIVED,
+                        DbContract.ServiceRuns.COLUMN_NAME_TOTAL_TRIGGERED},
+                new int[] {R.id.text_start_time,
+                        R.id.text_end_time,
+                        R.id.text_num_received,
+                        R.id.text_num_triggered}, 0);
 
-        Log.i(TAG,"Retrieving the recent history of service runs");
-        //todo: do this in a separate thread (AsyncTask)
-        myDb = new DbHelper(this).getReadableDatabase();
-        //todo: get the limit from shared preferences
-        Cursor c = ServiceRun.LatestRuns(myDb,10);
-        //todo: fix deprecated
-        ListAdapter adapter = new SimpleCursorAdapter(this,R.layout.service_run_record,c,
-                                                      new String[] {DbContract.ServiceRuns.COLUMN_NAME_START,
-                                                              DbContract.ServiceRuns.COLUMN_NAME_STOP,
-                                                              DbContract.ServiceRuns.COLUMN_NAME_TOTAL_RECEIVED,
-                                                              DbContract.ServiceRuns.COLUMN_NAME_TOTAL_TRIGGERED},
-                                                      new int[] {R.id.text_start_time,
-                                                                 R.id.text_end_time,
-                                                                 R.id.text_num_received,
-                                                                 R.id.text_num_triggered});
-        setListAdapter(adapter);
-        //end of todos
+        setListAdapter(myAdapter);
+        getLoaderManager().initLoader(URL_LOADER, null, this);
 
     }
 
     @Override
-    protected void onStop() {
+    public void onStop() {
         super.onStop();
-        myDb.close();
+        myDbConnection.close();
+    }
+    
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int loaderID, Bundle args) {
+        switch (loaderID) {
+            case URL_LOADER:
+                // Returns a new CursorLoader
+                return new CursorLoader(this,   // Parent activity context
+                                        null,   // All the following params
+                                        null,   // are null
+                                        null,   // because the Cursor is created
+                                        null,   // below
+                                        null)
+                {
+                    @Override
+                    public Cursor loadInBackground() {
+                        //todo: get the limit from shared preferences
+                        return ServiceRun.LatestRuns(myDbConnection, 10);
+                    }
+                };
+            default:
+                return null;
+        }
+    }
+
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        myAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        myAdapter.swapCursor(null);
     }
 }
